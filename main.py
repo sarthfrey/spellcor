318a31cd710195e8efb704c5f7f0c03d4abd194f
Sarth Frey <sarth.frey@shopify.com>
2016-06-14 21:50:44 -0400

    added view rendering form passing

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return "hello world"
+def index():
+	return render_template('index.html')
+
+@app.route('/correct', methods=['POST'])
+def correct():
+	word = request.form['correct']
+	return render_template('index.html', word=word)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	"""Return a custom 404 error."""
+	return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+	"""Return a custom 500 error."""
+	return 'Sorry, unexpected error: {}'.format(e), 500
 
 if __name__=="__main__":
 	app.run()
